74d57cfd405e3a197743e538efc65f550406fcdb
Isaiah Bernardino <isaiahkaibernardino@gmail.com>
2023-10-16 20:55:32 -0400

    pop up

--- src/pages/PlayerMainExperiencePage/PlayerMainExperiencePage.js
+++ src/pages/PlayerMainExperiencePage/PlayerMainExperiencePage.js
@@ -1,9 +1,21 @@
-function PlayerMainExperiencePage() {
-    return (
-      <div>
-        <h1>Player Main Experience Page</h1>
-      </div>
-    );
-  }
-  
-  export default PlayerMainExperiencePage;
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import BasicModal from '../../components/PopUp/PopUp';
+
+const PlayerMainExperiencePage = () => {
+  return (
+    <div>
+      <BasicModal modalHeader="You Died D:" modalBody="PLAYERNAME has killed you. Complete a task to revive yourself."/>
+    </div>
+  );
+};
+
+export default PlayerMainExperiencePage;
